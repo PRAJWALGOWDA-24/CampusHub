1a44a5cd250d0cf37ac76e2458f8084454c33bf7
PRAJWALGOWDA-24 <prajwal24afr@gmail.com>
2026-07-12 23:22:53 +0530

    adding back admin role and acess to admin

--- src/main/java/com/prajwal/campushub/controller/StudentController.java
+++ src/main/java/com/prajwal/campushub/controller/StudentController.java
@@ -32,6 +32,7 @@ public class StudentController {
 
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(
             summary = "Create Student",
             description = "Creates a new student in the CampusHub system"
